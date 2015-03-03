215f3ace0eac5bb9eba000a48b9de41caf70178f
Brandon DeRosier <bdero@mit.edu>
2015-03-02 22:50:48 -0500

    Click to create blocks and set as editable

--- blockparty.js
+++ blockparty.js
@@ -3,9 +3,27 @@ Blocks = new Mongo.Collection("blocks");
 if (Meteor.isClient) {
   Meteor.subscribe("blocks");
 
+  Session.set("editing", null);
+
   Template.content.helpers({
     blocks: function() {
       return Blocks.find({}).fetch();
+    },
+  });
+
+  Template.content.events({
+    'mousedown': function(e) {
+      // Create the block
+      var field_position = $(".field").position();
+      var block = Blocks.insert({
+        x: e.pageX - field_position.left,
+        y: e.pageY - field_position.top,
+        width: 0,
+        height: 0,
+      });
+
+      // Record the block we're currently editing
+      Session.set("editing", block);
     },
   });
 }

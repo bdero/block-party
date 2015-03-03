38517eeb6c84fcbc942c0500f63f63144e7b1c07
Brandon DeRosier <bdero@mit.edu>
2015-03-02 20:40:38 -0500

    Update and draw blocks

--- blockparty.js
+++ blockparty.js
@@ -2,6 +2,12 @@ Blocks = new Mongo.Collection("blocks");
 
 if (Meteor.isClient) {
   Meteor.subscribe("blocks");
+
+  Template.content.helpers({
+    blocks: function() {
+      return Blocks.find({}).fetch();
+    },
+  });
 }
 
 if (Meteor.isServer) {
